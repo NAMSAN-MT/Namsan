bb886ea471c2f72bde482221dca2c36fa94b99b0
김강현 <johnKim@estsoft.com>
2023-02-12 19:21:16 +0900

    [feat]: mixin 함수 추가

--- src/styles/mixin.style.ts
+++ src/styles/mixin.style.ts
@@ -1,6 +1,31 @@
 import { css } from "styled-components";
 
-export const size = css<{ height: string; width: string }>`
+export const setSize = css<{ height: string; width: string }>`
   height: ${(props) => props.height};
   width: ${(props) => props.width};
+`;
+
+export const setFlex = css<{ jc: string; ai: string }>`
+  display: flex;
+  justify-content: ${(props) => props.jc ?? "center"};
+  align-items: ${(props) => props.ai ?? "center"};
+`;
+
+export const setInlineblock = css<{ va: string }>`
+  display: inline-block;
+  vertical-align: ${(props) => props.va ?? "top"};
+`;
+
+export const setPosition = css<{
+  position: string;
+  top?: string;
+  right?: string;
+  bottom?: string;
+  left?: string;
+}>`
+  position: ${({ position }) => position};
+  top: ${({ top }) => top ?? null};
+  right: ${({ right }) => right ?? null};
+  bottom: ${({ bottom }) => bottom ?? null};
+  left: ${({ left }) => left ?? null};
 `;
