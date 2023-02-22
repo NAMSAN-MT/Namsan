b4ce36772dd17a72aa2717d3284bfb5a5e2455ca
sumini0829 <sumini0829@estsoft.com>
2023-02-23 01:22:38 +0900

    feat: icon 컴포넌트 추가

--- src/components/icons/LineArrowIcon/LineArrowIcon.tsx
+++ src/components/icons/LineArrowIcon/LineArrowIcon.tsx
@@ -1,84 +1,84 @@
-import React from "react";
-import IconWrapper from "../IconWrapper";
-import arrowUpTh15Icon from "../../../assets/imgs/ic_arrow_up_th15.svg";
-import arrowDownTh15Icon from "../../../assets/imgs/ic_arrow_down_th15.svg";
-import arrowUpTh10Icon from "../../../assets/imgs/ic_arrow_up_th10.svg";
-import arrowDownTh10Icon from "../../../assets/imgs/ic_arrow_down_th10.svg";
-import arrowLeftTh5Icon from "../../../assets/imgs/ic_arrow_left_th5.svg";
-import arrowLeftTh10Icon from "../../../assets/imgs/ic_arrow_left_th10.svg";
-import arrowLeftTh15Icon from "../../../assets/imgs/ic_arrow_left_th15.svg";
-import arrowRightTh5Icon from "../../../assets/imgs/ic_arrow_right_th5.svg";
-import arrowRightTh10Icon from "../../../assets/imgs/ic_arrow_right_th10.svg";
-import arrowRightTh15Icon from "../../../assets/imgs/ic_arrow_right_th15.svg";
+import React from 'react';
+import IconWrapper from '../IconWrapper';
+import arrowUpTh15Icon from '../../../assets/imgs/ic_arrow_up_th15.svg';
+import arrowDownTh15Icon from '../../../assets/imgs/ic_arrow_down_th15.svg';
+import arrowUpTh10Icon from '../../../assets/imgs/ic_arrow_up_th10.svg';
+import arrowDownTh10Icon from '../../../assets/imgs/ic_arrow_down_th10.svg';
+import arrowLeftTh5Icon from '../../../assets/imgs/ic_arrow_left_th5.svg';
+import arrowLeftTh10Icon from '../../../assets/imgs/ic_arrow_left_th10.svg';
+import arrowLeftTh15Icon from '../../../assets/imgs/ic_arrow_left_th15.svg';
+import arrowRightTh5Icon from '../../../assets/imgs/ic_arrow_right_th5.svg';
+import arrowRightTh10Icon from '../../../assets/imgs/ic_arrow_right_th10.svg';
+import arrowRightTh15Icon from '../../../assets/imgs/ic_arrow_right_th15.svg';
 
 interface PropTypes {
   width?: string;
   height?: string;
-  direction: "UP" | "DOWN" | "LEFT" | "RIGHT";
-  weight: "BOLD" | "NORMAL" | "LIGHT";
+  direction: 'UP' | 'DOWN' | 'LEFT' | 'RIGHT';
+  weight: 'BOLD' | 'NORMAL' | 'LIGHT';
 }
 
 const LineArrowIcon = (props: PropTypes) => {
   let src, alt;
 
-  if (props.direction === "UP") {
+  if (props.direction === 'UP') {
     switch (props.weight) {
-      case "BOLD":
+      case 'BOLD':
         src = arrowUpTh15Icon;
-        alt = "arrow up th15 icon";
+        alt = 'arrow up th15 icon';
         break;
-      case "NORMAL":
+      case 'NORMAL':
       default:
         src = arrowUpTh10Icon;
-        alt = "arrow up th10 icon";
+        alt = 'arrow up th10 icon';
         break;
     }
-  } else if (props.direction === "DOWN") {
+  } else if (props.direction === 'DOWN') {
     switch (props.weight) {
-      case "BOLD":
+      case 'BOLD':
         src = arrowDownTh15Icon;
-        alt = "arrow down th15 icon";
+        alt = 'arrow down th15 icon';
         break;
-      case "NORMAL":
+      case 'NORMAL':
       default:
         src = arrowDownTh10Icon;
-        alt = "arrow down th10 icon";
+        alt = 'arrow down th10 icon';
         break;
     }
-  } else if (props.direction === "LEFT") {
+  } else if (props.direction === 'LEFT') {
     switch (props.weight) {
-      case "BOLD":
+      case 'BOLD':
         src = arrowLeftTh15Icon;
-        alt = "arrow left th15 icon";
+        alt = 'arrow left th15 icon';
         break;
-      case "LIGHT":
+      case 'LIGHT':
         src = arrowLeftTh5Icon;
-        alt = "arrow left th5 icon";
-      case "NORMAL":
+        alt = 'arrow left th5 icon';
+      case 'NORMAL':
       default:
         src = arrowLeftTh10Icon;
-        alt = "arrow left th10 icon";
+        alt = 'arrow left th10 icon';
         break;
     }
-  } else if (props.direction === "RIGHT") {
+  } else if (props.direction === 'RIGHT') {
     switch (props.weight) {
-      case "BOLD":
+      case 'BOLD':
         src = arrowRightTh15Icon;
-        alt = "arrow right th15 icon";
+        alt = 'arrow right th15 icon';
         break;
-      case "LIGHT":
+      case 'LIGHT':
         src = arrowRightTh5Icon;
-        alt = "arrow right th5 icon";
-      case "NORMAL":
+        alt = 'arrow right th5 icon';
+      case 'NORMAL':
       default:
         src = arrowRightTh10Icon;
-        alt = "arrow right th10 icon";
+        alt = 'arrow right th10 icon';
         break;
     }
   }
 
   return (
-    <IconWrapper width={props.width || "24px"} height={props.height || "24px"}>
+    <IconWrapper width={props.width || '24px'} height={props.height || '24px'}>
       <img src={src} alt={alt} />
     </IconWrapper>
   );
