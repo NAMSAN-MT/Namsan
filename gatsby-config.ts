1282d9b92dab63567d132f6594651d5408d405f8
sumini0829 <sumini0829@estsoft.com>
2023-02-23 20:53:35 +0900

    [feat]: intl 적용 및 가이드 추가 (i18n)

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,66 +1,77 @@
-import type { GatsbyConfig } from "gatsby";
-import { resolve, join } from "path";
+import type { GatsbyConfig } from 'gatsby';
+import { resolve, join } from 'path';
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Namsan`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, 
-  "gatsby-plugin-mdx", 
-  "gatsby-plugin-sharp", 
-  "gatsby-transformer-sharp", 
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      path: join(__dirname, `src`, `images`),
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
     },
-    __key: "pages"
-  },
-  {
-    resolve: `gatsby-plugin-alias-imports`,
-    options: {
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/',
+        __key: 'pages',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-alias-imports`,
+      options: {
         alias: {
-            "@Api": resolve(__dirname, 'src/api'),
-            "@Components": resolve(__dirname, 'src/components'),
-            "@Images": resolve(__dirname, 'src/images'),
-            "@Fonts": resolve(__dirname, 'src/fonts'),
-            "@Interface": resolve(__dirname, 'src/interface'),
-            "@Pages": resolve(__dirname, 'src/pages'),
-            "@Styles": resolve(__dirname, 'src/styles')
+          '@Api': resolve(__dirname, 'src/api'),
+          '@Components': resolve(__dirname, 'src/components'),
+          '@Images': resolve(__dirname, 'src/images'),
+          '@Fonts': resolve(__dirname, 'src/fonts'),
+          '@Interface': resolve(__dirname, 'src/interface'),
+          '@Pages': resolve(__dirname, 'src/pages'),
+          '@Styles': resolve(__dirname, 'src/styles'),
         },
-        extensions: [
-            "ts",
-            "tsx"
-        ]
-      }
-  },
-  {
-    resolve: "gatsby-plugin-react-svg",
-    options: {
-      rule: {
-        include: /images\/svg/,
-      }
-    }
-  },
-]
+        extensions: ['ts', 'tsx'],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /images\/svg/,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-intl',
+      options: {
+        path: `${__dirname}/src/intl`,
+        languages: [`en`, `ko`],
+        defaultLanguage: `ko`,
+        redirect: false,
+      },
+    },
+  ],
 };
 
 export default config;
