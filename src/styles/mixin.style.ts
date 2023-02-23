3abff2e75d0aa9d5775bc4e521cdf535b740fcf3
김강현 <johnKim@estsoft.com>
2023-02-23 19:16:02 +0900

    [style]: 일부 파일 prettier 스타일 적용

--- src/styles/mixin.style.ts
+++ src/styles/mixin.style.ts
@@ -1,25 +1,25 @@
-import { css } from "styled-components";
+import { css } from 'styled-components'
 
-export const size = (height: string = "auto", width: string = "auto") => css`
+export const size = (height: string = 'auto', width: string = 'auto') => css`
   height: ${height};
   width: ${width};
-`;
+`
 
-export const flex = (jc: string = "center", ai: string = "center") => css`
+export const flex = (jc: string = 'center', ai: string = 'center') => css`
   display: flex;
   justify-content: ${jc};
   align-items: ${ai};
-`;
+`
 
-export const inlineblock = (va: string = "top") => css`
+export const inlineblock = (va: string = 'top') => css`
   display: inline-block;
   vertical-align: ${va};
-`;
+`
 
-export const inlineblockJustFunction = (va: string = "top") => css`
+export const inlineblockJustFunction = (va: string = 'top') => css`
   display: inline-block;
   vertical-align: ${va};
-`;
+`
 
 export const position = (
   $position: string,
@@ -33,11 +33,11 @@ export const position = (
   right: ${right};
   bottom: ${bottom};
   left: ${left};
-`;
+`
 
 export const ellipsis = () => css`
   overflow: hidden;
   white-space: nowrap;
   -ms-text-overflow: ellipsis;
   text-overflow: ellipsis;
-`;
+`
