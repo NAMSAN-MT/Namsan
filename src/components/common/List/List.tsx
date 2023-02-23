d5798d7179b5d586b66b1789040b466118d1a78a
김강현 <johnKim@estsoft.com>
2023-02-23 21:58:28 +0900

    [style]: List 컴포넌트 역할별 분리

--- src/components/common/List/List.tsx
+++ src/components/common/List/List.tsx
@@ -1,85 +1,32 @@
-import React from "react";
-import styled from "styled-components";
-
-type MainCategory = {
-  id: string;
-  name: string;
-  children?: ReturnType<typeof SubCategory>[];
-};
-
-type SubCategory = {
-  subId: string;
-  name: string;
-};
-
-const List = styled.ul`
-  margin: 0px;
-  padding: 0px;
-`;
-
-const ListItem = styled.li`
-  list-style-type: none;
-`;
-
-const HR = styled.hr`
-  border: 0;
-  height: 2px;
-  background: ${({ theme }) => theme.color.dividerBlack};
-`;
-
-const Anchor = styled.a`
-  text-decoration: none;
-  color: ${({ theme }) => theme.color.textBlackHigh};
-  height: auto;
-  padding: 13px 12px;
-
-  font-size: 20px;
-
-  display: block;
-  &:hover {
-    background-color: ${({ theme }) => theme.color.grey100};
-  }
-`;
-
-const MainAnchor = styled(Anchor)`
-  font-weight: 700;
-  font-size: 24px;
-  line-height: 38px;
-  &:hover {
-    color: ${({ theme }) => theme.color.blue100};
-    background-color: transparent;
-  }
-`;
-
-const Wrapper = styled.div`
-  margin: 12px;
-`;
+import React from 'react'
+import { Wrapper, MainAnchor, HR, List, ListItem, Anchor } from './List.style'
+import { MainCategoryType, SubCategoryType } from './List.type'
 
 // TODO: 아이콘 추가 필요
-const MainCategory = ({ id, name, children }: MainCategory) => {
+export const MainCategory = ({ id, name, children }: MainCategoryType) => {
   return (
     <Wrapper>
       <MainAnchor href={id}>{name}</MainAnchor>
       <HR />
       <List>
-        {children?.map((item) => ({
+        {children?.map(item => ({
           ...item,
           props: { ...item.props, subId: `${id}/${item.props.subId}` },
         }))}
       </List>
     </Wrapper>
-  );
-};
+  )
+}
 
-const SubCategory = ({ name, subId }: SubCategory) => {
+export const SubCategory = ({ name, subId }: SubCategoryType) => {
   return (
     <ListItem>
       <Anchor href={subId}>{name}</Anchor>
     </ListItem>
-  );
-};
+  )
+}
 
 export const LE = {
   MainCategory,
   SubCategory,
-};
+}
