29e9127dfb01a2383a0e8c474d7600e8cecda71c
sumini0829 <sumini0829@estsoft.com>
2023-02-23 20:53:35 +0900

    [feat]: intl 적용 및 가이드 추가 (i18n)

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -2,9 +2,9 @@ import {
   FormattedMessage,
   injectIntl,
   WrappedComponentProps,
-} from "gatsby-plugin-intl";
-import React from "react";
-import HomeIcon from "../components/icons/HomeIcon";
+} from 'gatsby-plugin-intl';
+import React from 'react';
+import HomeIcon from '../components/icons/HomeIcon';
 
 // TODO: WrappedComponentProps를 프롭스에 implement해서 사용하기 (hoc에서 intl속성을 내려줌.)
 const TestPage = ({ intl }: WrappedComponentProps) => {
@@ -14,15 +14,15 @@ const TestPage = ({ intl }: WrappedComponentProps) => {
       <h1>✅ Intl 적용 방법1</h1>
       <div>
         {intl.formatMessage(
-          { id: "index.test" },
-          { myValue: "여기에 변수를 추가할수도 있습니다!" }
+          { id: 'index.test' },
+          { myValue: '여기에 변수를 추가할수도 있습니다!' },
         )}
       </div>
       <h1>✅ Intl 적용 방법2</h1>
       <div>
         <FormattedMessage
           id="index.test"
-          values={{ myValue: "여기는 또 다른 변수를 추가할 수도 있습니당" }}
+          values={{ myValue: '여기는 또 다른 변수를 추가할 수도 있습니당' }}
         />
       </div>
     </main>
