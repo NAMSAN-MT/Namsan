cb51ace0872c71b7e66f88f84f2054e92a5b6656
Santos <wjdrms1919@gmail.com>
2023-03-11 17:07:07 +0900

    [FEAT] lazy, suspense 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,21 @@ import AppLayout from '@Components/common/Layout';
 import FirstSection from '@Components/main/FirstSection';
 import SecondSection from '@Components/main/SecondSection';
 import ThirdSection from '@Components/main/ThirdSection';
-import ForthSection from '@Components/main/ForthSection';
+const ForthSection = React.lazy(() => import("@Components/main/ForthSection"))
 import FifthSection from '@Components/main/FifthSection';
 
 const Main: React.FC<PageProps> = () => {
+  const isSSR = typeof window === "undefined"
   return (
     <AppLayout>
       <FirstSection />
       <SecondSection />
       <ThirdSection />
-      <ForthSection />
+      {!isSSR && (
+        <React.Suspense fallback={<div>loading</div>}>
+          <ForthSection />
+        </React.Suspense>
+      )}
       <FifthSection />
     </AppLayout>
   );
