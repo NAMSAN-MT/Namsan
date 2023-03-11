6603b6a01454ef317ac3082df56e5d58b99a33ce
Santos <wjdrms1919@gmail.com>
2023-03-11 15:56:23 +0900

    [FIX] 경로 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,24 +15,6 @@ const Main: React.FC<PageProps> = () => {
       <ThirdSection />
       <ForthSection />
       <FifthSection />
-      {/* <BaseButton className="primary" onClick={() => console.log('')}>
-        Primary
-      </BaseButton>
-      <BaseButton className="support" onClick={() => console.log('')}>
-        Support
-      </BaseButton>
-      <BaseButton className="support-line" onClick={() => console.log('')}>
-        SupportLine
-      </BaseButton>
-      <BaseButton className="outline" onClick={() => console.log('')}>
-        Outline
-      </BaseButton>
-      <BaseButton className="tag" onClick={() => console.log('')}>
-        Tag
-      </BaseButton>
-      <IconButton className="arrow-top" onClick={() => console.log('')} />
-      <IconButton className="hamburger" onClick={() => console.log('')} />
-      <IconButton className="share" onClick={() => console.log('')} /> */}
     </AppLayout>
   );
 };
