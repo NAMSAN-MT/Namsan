e231000b43c85abc90a1a434a1960594806edf3d
Santos <wjdrms1919@gmail.com>
2023-03-11 17:07:59 +0900

    [FEAT] 파이어베이스에서 실 데이터 가져오기

--- src/components/main/FifthSection/FifthSection.hook.ts
+++ src/components/main/FifthSection/FifthSection.hook.ts
@@ -3,8 +3,13 @@ import useNavigate from '../../../hooks/useNavigate';
 const useFifthSection = () => {
   const { handleNavigate } = useNavigate();
 
+   const handleNavigateTo = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    handleNavigate('work');
+  };
+
   return {
-    handleNavigate,
+    handleNavigateTo,
   };
 };
 
