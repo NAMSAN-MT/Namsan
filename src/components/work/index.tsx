d052ff5bdb25245fea333f92afec700fb918ea4c
김강현 <johnKim@estsoft.com>
2023-03-22 05:05:15 +0900

    [feat]: 디테일 페이지 추가

--- src/components/work/index.tsx
+++ src/components/work/index.tsx
@@ -1,24 +1,26 @@
 import { getWorkFields } from '@Api/work.api';
 import { Container } from '@Components/common/Container/Container';
 import { LE } from '@Components/common/List';
+import { RouteComponentProps } from '@reach/router';
 import React, { useEffect, useState } from 'react';
 import { Category } from './work.interface';
 import { Grid } from './work.styled';
-import { RouteComponentProps } from '@reach/router';
 
 export interface Props extends RouteComponentProps {}
 
 const Work = (props: Props) => {
   const [mainCategory, setMainCategory] = useState<Category[]>();
   useEffect(() => {
-    getWorkFields().then(result => setMainCategory(result));
+    getWorkFields(['categoryInfo']).then(result => {
+      setMainCategory(result);
+    });
   }, []);
   return (
     <Container title="업무분야">
       <Grid>
-        {mainCategory?.map(({ categoryId: id, name, category }) => (
+        {mainCategory?.map(({ categoryId: id, name, subCategory }) => (
           <LE.MainCategory id={id} name={name} key={id}>
-            {category.map(({ categoryId: subId, name: subName }) => (
+            {subCategory?.map(({ categoryId: subId, name: subName }) => (
               <LE.SubCategory subId={subId} name={subName} key={subId} />
             ))}
           </LE.MainCategory>
