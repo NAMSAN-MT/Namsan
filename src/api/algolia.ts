ac3cb489847ac11bf9dd97dd0262569c5115efbb
devTap <rlaxogus0517@gmail.com>
2023-04-23 22:00:18 +0900

    긴급 머지

--- src/api/algolia.ts
+++ src/api/algolia.ts
@@ -1,6 +1,6 @@
 import algoliasearch from 'algoliasearch/lite';
 
-export const INDEX_NAME = process.env.ALGOLIA_INDEX_NAME ?? '';
+export const INDEX_NAME = process.env.GATSBY_ALGOLIA_INDEX_NAME ?? '';
 export const algoliaClient = algoliasearch(
   process.env.GATSBY_ALGOLIA_ID!,
   process.env.GATSBY_ALGOLIA_SEARCH_KEY!,
