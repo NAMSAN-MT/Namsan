e581105ef913a5ff196df7fdc0c4c1d6e9e9d1b8
Santos <wjdrms1919@gmail.com>
2023-04-24 21:32:59 +0900

    [FIX] console.log 삭제

--- src/components/main/ForthSection/ForthSection.tsx
+++ src/components/main/ForthSection/ForthSection.tsx
@@ -14,9 +14,6 @@ const ForthSection = (props: IForthSectionProps) => {
   const { handleNavigateTo, newsList } = useForthSection();
   const { convertToDateString } = useDateFns();
   const threshold = props.isMobile ? 0.2 : props.isTablet ? 0 : 0.5;
-
-  console.log(props, '--');
-
   return (
     <AnimationWrapper
       variantName="transition"
