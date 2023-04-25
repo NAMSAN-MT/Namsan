8914655aa2ae18612e6175b00a1074d89d4b8483
sumini0829 <sumini0829@estsoft.com>
2023-04-26 00:18:11 +0900

    [FIX]: SSR동작시 하이드레이션 에러 수정

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,13 +1,45 @@
 import { resolve } from 'path';
 
-exports.createPages = async ({ actions }: { actions: any }) => {
-  actions.createPage({
-    path: '/member/1',
-    component: resolve('./src/pages/member/[id].tsx'),
+exports.createPages = async ({ actions, graphql }: any) => {
+  const members = await graphql(`
+    query {
+      allMembers {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+  `);
+  members.data.allMembers.edges.forEach(({ node }: any) => {
+    actions.createPage({
+      path: `/member/${node.id}`,
+      component: resolve('./src/pages/member/[id].tsx'),
+      context: {
+        id: node.id,
+      },
+    });
   });
 
-  actions.createPage({
-    path: '/work/C01',
-    component: resolve('./src/pages/work/[id].tsx'),
+  const works = await graphql(`
+    query {
+      allWork {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+  `);
+  works.data.allWork.edges.forEach(({ node }: any) => {
+    actions.createPage({
+      path: `/work/${node.id}`,
+      component: resolve('./src/pages/work/[id].tsx'),
+      context: {
+        id: node.id,
+      },
+    });
   });
 };
--- src/pages/member/[id].tsx
+++ src/pages/member/[id].tsx
@@ -3,9 +3,8 @@ import { PageProps } from 'gatsby';
 import Member from '@Components/member/Member';
 import Layout from '@Components/common/Layout';
 
-const MemberPage: React.FC<PageProps> = ({ params }) => {
-  const { id: idStr } = params;
-  const id = Number(idStr);
+const MemberPage: React.FC<PageProps> = ({ pageContext }) => {
+  const { id } = pageContext as { id: string };
 
   return (
     <Layout>
