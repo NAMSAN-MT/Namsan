59cc78971c1735ce186b12eeff448dab07931dc2
devTap <rlaxogus0517@gmail.com>
2023-04-25 22:19:15 +0900

    [FIX] 페이지네이션 케이스별 로직 처리 완료

--- src/components/news/Pagination/Pagination.helper.ts
+++ src/components/news/Pagination/Pagination.helper.ts
@@ -1,9 +1,17 @@
 import { NewsType } from '@Type/api.type';
 import { isEmpty } from 'lodash';
+import { URLSearchParams } from 'url';
+
+export const getPageList = (length: number) => Array.from({ length });
 
 export const toQuery = (newsType: NewsType, page?: number) => {
   const toNewsType =
     newsType === 'all' || isEmpty(newsType) ? '' : `newsType=${newsType}&`;
   const toPage = page ? `page=${page}` : '';
   return `?${toNewsType}${toPage}`;
+};
+
+export const getNewQueryString = (params: URLSearchParams, page: number) => {
+  params.set('page', String(page));
+  return String(params);
 };
