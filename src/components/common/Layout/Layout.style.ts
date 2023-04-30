209b1f6e28a25a9727de8b26a38b131e3a8325d7
devTap <rlaxogus0517@gmail.com>
2023-05-01 01:53:55 +0900

    [FEATURE] Layout>Floating 영역 추가

--- src/components/common/Layout/Layout.style.ts
+++ src/components/common/Layout/Layout.style.ts
@@ -1,5 +1,12 @@
-import { flex, mediaQuery } from '@Styles/mixin.style';
-import styled from 'styled-components';
+import { font } from './../../../styles/mixin.style';
+import {
+  flex,
+  flexDirection,
+  mediaQuery,
+  size,
+  lineHeight,
+} from '@Styles/mixin.style';
+import styled, { keyframes } from 'styled-components';
 
 const LayoutWrapper = styled.div`
   height: 100%;
@@ -42,4 +49,85 @@ const TopButtonWrapper = styled.div`
   )};
 `;
 
-export { LayoutWrapper, LayoutContent, TopButtonWrapper };
+const FloatingWrapper = styled.div`
+  width: 100%;
+  ${flex('space-between', 'center ')};
+  position: fixed;
+  right: 90px;
+  bottom: 284px;
+
+  .blank {
+  }
+  .area {
+    width: 60px;
+    ${flexDirection('column')}
+    gap: 12px;
+  }
+
+  ${mediaQuery(
+    'tablet1024',
+    `
+     right: 40px;
+     bottom: 276px;
+    `,
+  )};
+  ${mediaQuery(
+    'mobile',
+    `
+     right: 24px;
+     bottom: 266px;
+    `,
+  )};
+`;
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
+
+const ToastWrapper = styled.div<{ isVisible: boolean }>`
+  ${flex()}
+  ${size('48px', '100%')}
+  position: fixed;
+  bottom: 284px;
+  visibility: ${({ isVisible }) => (isVisible ? 'visible' : 'hidden')};
+  animation: ${({ isVisible }) => (isVisible ? fadeIn : fadeOut)} 0.45s ease-out;
+  transition: visibility 0.45s ease-out;
+`;
+
+const Toast = styled.span`
+  ${flex()}
+  ${size('48px', '320px')}
+
+  background: ${({ theme }) => theme.color.black};
+  opacity: 0.85;
+  border-radius: 10px;
+
+  ${font('body16', 'regular')}
+  ${lineHeight(16, 26)}
+  text-align: center;
+  letter-spacing: -0.2px;
+  color: ${({ theme }) => theme.color.white};
+`;
+
+export {
+  LayoutWrapper,
+  LayoutContent,
+  TopButtonWrapper,
+  FloatingWrapper,
+  ToastWrapper,
+  Toast,
+};
