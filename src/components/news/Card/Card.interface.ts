673bc37e1d24be9c625061694831e06c83e4a8e1
devTap <93532696+Tap-Kim@users.noreply.github.com>
2023-05-01 18:55:26 +0900

    메인 영역 뉴스 조회 오류 수정 (#59)
    * [FIX] 메인 뉴스 영역 오류 수정
    
    * [FIX] 조회시 documentId 포함

--- src/components/news/Card/Card.interface.ts
+++ src/components/news/Card/Card.interface.ts
@@ -2,9 +2,11 @@ import { News } from '@Interface/api.interface';
 import { NewsType } from '@Type/api.type';
 
 export interface CardProps {
+  type: 'main' | 'news';
   newsList: News[];
-  urlPage: number;
-  newsType: NewsType;
-  searchValue: string;
-  onCallNewsList: (newsType: NewsType, searchValue?: string) => Promise<void>;
+  urlPage?: number;
+  newsType?: NewsType;
+  searchValue?: string;
+  onCallNewsList?: (newsType?: NewsType, searchValue?: string) => Promise<void>;
+  onCallMainNewsList?: (limit: number) => void;
 }
