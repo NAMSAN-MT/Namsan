6ca169e3de0da1a94762d5d7e78cbe871901dcac
sumini0829 <sumini0829@estsoft.com>
2023-05-02 00:35:40 +0900

    [FEAT] 언어별 검색 가능하도록 수정

--- src/templates/members.tsx
+++ src/templates/members.tsx
@@ -5,17 +5,25 @@ import MembersTitle from '@Components/members/MembersTitle';
 import SearchBar from '@Components/members/SearchBar';
 import MemberList from '@Components/members/MemberList';
 import Layout from '@Components/common/Layout';
+import { WrappedComponentProps, injectIntl } from 'gatsby-plugin-intl';
+import { IMember } from '@Interface/api.interface';
 
-const Members: React.FC<PageProps> = ({ pageContext: { members } }: any) => {
+const Members: React.FC<PageProps & WrappedComponentProps> = ({
+  pageContext: { members },
+  intl,
+}: any) => {
+  const filteredMembersWithLanguage = members.filter(
+    (member: IMember) => member.language === intl.locale,
+  );
   return (
     <Layout>
       <MembersWrapper>
         <MembersTitle />
-        <SearchBar members={members} />
-        <MemberList members={members} />
+        <SearchBar members={filteredMembersWithLanguage} />
+        <MemberList members={filteredMembersWithLanguage} />
       </MembersWrapper>
     </Layout>
   );
 };
 
-export default Members;
+export default injectIntl(Members);
