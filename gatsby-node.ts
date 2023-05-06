945cc53e172655444749a746f05caadc8f6cf8be
devTap <rlaxogus0517@gmail.com>
2023-05-06 21:48:57 +0900

    [FIX] news/[id] graphql gatsby-node로 가져오기 변경

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -241,6 +241,13 @@ exports.createPages = async ({ actions, graphql }: any) => {
         edges {
           node {
             id
+            newsType
+            originalLink
+            imagePath
+            title
+            date
+            content
+            agency
           }
         }
       }
@@ -251,7 +258,7 @@ exports.createPages = async ({ actions, graphql }: any) => {
       path: `/news/${node.id}`,
       component: resolve('./src/pages/news/[id].tsx'),
       context: {
-        id: node.id,
+        news: { ...node },
       },
     });
   });
--- src/pages/news/[id].tsx
+++ src/pages/news/[id].tsx
@@ -2,35 +2,28 @@ import Layout from '@Components/common/Layout';
 import Loading from '@Components/common/Loading';
 import NewsWrapper from '@Components/news/NewsWrapper';
 import { News } from '@Interface/api.interface';
-import { graphql, PageProps } from 'gatsby';
+import { PageProps } from 'gatsby';
+import { injectIntl, WrappedComponentProps } from 'gatsby-plugin-intl';
 import React, { lazy, Suspense } from 'react';
 const NewsDetail = lazy(() => import('@Components/news/NewsDetail/NewsDetail'));
 
-const NewsDatil: React.FC<PageProps & News> = ({ data }) => {
-  const newsData = (data as any).news;
+interface _PageProps extends PageProps {
+  pageContext: { news: News };
+}
 
+const NewsDatil: React.FC<_PageProps & WrappedComponentProps> = ({
+  pageContext: { news },
+  intl,
+}) => {
   return (
     <Layout route="newsDetail">
       <NewsWrapper outerPadding="100px 90px 160px" innerWidth="996px">
         <Suspense fallback={<Loading height="500px" />}>
-          <NewsDetail {...newsData} />
+          <NewsDetail {...news} />
         </Suspense>
       </NewsWrapper>
     </Layout>
   );
 };
 
-export default NewsDatil;
-
-export const query = graphql`
-  query ($id: String) {
-    news(id: { eq: $id }) {
-      newsType
-      originalLink
-      title
-      date
-      content
-      agency
-    }
-  }
-`;
+export default injectIntl(NewsDatil);
