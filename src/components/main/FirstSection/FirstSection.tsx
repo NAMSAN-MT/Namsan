a8f8263a67769a294d3f93a6a3266d439a4ad351
Santos <wjdrms1919@gmail.com>
2023-05-31 14:08:23 +0900

    [FIX] HOTFIX 수정

--- src/components/main/FirstSection/FirstSection.tsx
+++ src/components/main/FirstSection/FirstSection.tsx
@@ -22,94 +22,90 @@ const FirstSection = (props: IFirstSectionProps) => {
   }, [inView, props.isDesktop]);
 
   return (
-    <>
-      <S.FrirstWrapper ref={ref}>
-        {!startSlide ? (
-          <S.FrirstWrapper>
-            <img
-              width={props.isMobile ? '100%' : 'auto'}
-              src={intro}
-              alt="intro0"
-            />
-          </S.FrirstWrapper>
+    <S.FrirstWrapper ref={ref}>
+      <div
+        style={{
+          width: '100%',
+          height: !props.isMobile ? '760px' : '560px',
+          position: !startSlide ? 'static' : 'absolute',
+          opacity: !startSlide ? 1 : 0,
+          transition: 'opacity 1s ease-in',
+        }}
+      >
+        <S.FrirstWrapper>
+          <img
+            width={props.isMobile ? '100%' : 'auto'}
+            src={intro}
+            alt="intro0"
+          />
+        </S.FrirstWrapper>
+      </div>
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          position: startSlide ? 'static' : 'absolute',
+          opacity: startSlide ? 1 : 0,
+          transition: 'opacity 1s ease-in',
+        }}
+      >
+        <Carousel
+          animationHandler="fade"
+          showArrows={false}
+          showStatus={false}
+          showIndicators={false}
+          showThumbs={false}
+          autoPlay={startSlide}
+          infiniteLoop={true}
+          stopOnHover={false}
+          swipeable={false}
+          dynamicHeight={false}
+          width={'100%'}
+          emulateTouch={false}
+          interval={5000}
+          transitionTime={1000}
+        >
+          {pictures.map((picture, index) => (
+            <S.ScaleWrapper key={index}>
+              <img
+                height={!props.isMobile ? '100%' : 'auto'}
+                src={picture}
+                alt={`intro ${index}`}
+              />
+            </S.ScaleWrapper>
+          ))}
+        </Carousel>
+      </div>
+      <S.TextWrapper
+        style={{
+          visibility: startSlide ? 'visible' : 'hidden',
+          position: 'absolute',
+          opacity: startSlide ? 1 : 0,
+          transition: 'opacity 1s ease-in',
+        }}
+      >
+        {props.isMobile ? (
+          <>
+            <S.Title>
+              {props.intl.formatMessage({
+                id: `main.title1`,
+              })}
+            </S.Title>
+            <S.SubTitle>Lim, Chung & Suh</S.SubTitle>
+          </>
         ) : (
           <>
-            <AnimatePresence mode="wait">
-              <motion.div
-                style={{
-                  width: '100%',
-                  height: '100%',
-                }}
-                initial={{ opacity: 0 }}
-                animate={{
-                  opacity: 1,
-                  transition: { duration: 1 },
-                }}
-                exit={{ opacity: 0, transition: { duration: 1 } }}
-                key={`intro`}
-              >
-                <Carousel
-                  animationHandler="fade"
-                  showArrows={false}
-                  showStatus={false}
-                  showIndicators={false}
-                  showThumbs={false}
-                  autoPlay={true}
-                  infiniteLoop={true}
-                  stopOnHover={false}
-                  swipeable={false}
-                  dynamicHeight={false}
-                  width={'100%'}
-                  emulateTouch={false}
-                  interval={5000}
-                  transitionTime={1000}
-                >
-                  {pictures.map((picture, index) => (
-                    <S.ScaleWrapper key={index}>
-                      <img
-                        height={!props.isMobile ? '100%' : 'auto'}
-                        src={picture}
-                        alt={`intro ${index}`}
-                      />
-                    </S.ScaleWrapper>
-                  ))}
-                </Carousel>
-              </motion.div>
-            </AnimatePresence>
-            <S.TextWrapper
-              animate={{
-                opacity: 1,
-                translateY: [20, 0],
-                transition: {
-                  duration: 3,
-                },
-              }}
-            >
-              {props.isMobile ? (
-                <>
-                  <S.Title>
-                    {props.intl.formatMessage({
-                      id: `main.title1`,
-                    })}
-                  </S.Title>
-                  <S.SubTitle>Lim, Chung & Suh</S.SubTitle>
-                </>
-              ) : (
-                <>
-                  <S.Title>Namsan</S.Title>
-                  <S.SubTitle>Lim, Chung & Suh</S.SubTitle>
-                  <S.Description>
-                    {props.intl.formatMessage({
-                      id: `main.title1`,
-                    })}
-                  </S.Description>
-                </>
-              )}
-            </S.TextWrapper>
+            <S.Title>Namsan</S.Title>
+            <S.SubTitle>Lim, Chung & Suh</S.SubTitle>
+            <S.Description>
+              {props.intl.formatMessage({
+                id: `main.title1`,
+              })}
+            </S.Description>
           </>
         )}
-      </S.FrirstWrapper>
-    </>
+      </S.TextWrapper>
+    </S.FrirstWrapper>
   );
 };
 
