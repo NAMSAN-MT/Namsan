2bca8ec41deb4586c94b45def8add971973b7865
Santos <wjdrms1919@gmail.com>
2023-06-25 16:45:14 +0900

    [FIX] 남산 소식 영문 추가

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,16 +1,16 @@
 import Layout from '@Components/common/Layout';
 import NewsMain from '@Components/news/Main';
 import NewsWrapper from '@Components/news/NewsWrapper';
-import { NewsType } from '@Type/api.type';
-import { GetServerDataProps, PageProps } from 'gatsby';
+import { WrappedComponentProps, injectIntl } from 'gatsby-plugin-intl';
 import * as React from 'react';
-import styled from 'styled-components';
 
-const Main: React.FC<PageProps> = () => {
+const Main = (props: WrappedComponentProps) => {
   return (
     <Layout route="news">
       <NewsWrapper
-        title="남산소식"
+        title={props.intl.formatMessage({
+          id: 'common.news',
+        })}
         outerPadding="100px 90px 160px;"
         innerWidth="1200px"
       >
@@ -20,4 +20,4 @@ const Main: React.FC<PageProps> = () => {
   );
 };
 
-export default Main;
+export default injectIntl(Main);
